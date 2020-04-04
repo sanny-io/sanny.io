280feeb549dff71df1b6d10e8fb089c77e2b5dbe
sanny-io <sannysherief@gmail.com>
2020-04-03 23:55:41 -0700

    Add scroll memory.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollMemory from 'react-router-scroll-memory';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import {
@@ -16,6 +17,7 @@ import DetailedProject from '../Projects/DetailedProject';
 export default function App({ classes }) {
   return (
     <Router>
+      <ScrollMemory />
       <div className="flex flex-col h-full lg:flex-row">
         <aside className="lg:w-1/4 w-full lg:fixed">
           <PersonalCard />
