394cebb558cf0c17c6c44ef1cc564dfec7c03042
sanny-io <sannysherief@gmail.com>
2021-05-28 03:39:48 -0700

    Use polyfill.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgr from 'vite-plugin-svgr';
 import markdown, { Mode } from 'vite-plugin-markdown';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
     svgr(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
     markdown({
       mode: [Mode.REACT],
     }),
