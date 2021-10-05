52c574a2779682eb38c8685441cecf66e1991e8c
sanny-io <sannysherief@gmail.com>
2021-10-04 19:52:08 -0700

    New mock photo.

--- components/Design/index.tsx
+++ components/Design/index.tsx
@@ -9,20 +9,22 @@ export type DesignProps = {
 
 export default function Design({ children }: DesignProps) {
   return (
-    <section className="pb-8 text-white bg-primary md:pb-32">
+    <section className="pb-2 text-white bg-primary">
       {/* <div className="container flex flex-col px-4 mx-auto lg:flex-row lg:space-x-14"> */}
       <div className="container flex flex-col px-4 mx-auto lg:flex-row lg:space-x-14">
-        <div className="self-center w-full mb-8 lg:w-1/2 md:mb-16">
+        <div className="self-center w-1/2 mb-8 lg:w-1/3 md:mb-16">
           <Image
             src={mock}
             placeholder="blur"
             layout="responsive"
             className="duration-200"
-            alt="Sample site as viewed on various devices"
+            alt="Sample site as viewed on a mobile phone"
           />
         </div>
-        <div className="lg:w-1/2">
+
+        <div className="w-full lg:w-2/3">
           <h2 className="mb-8 text-5xl font-light uppercase md:text-6xl">Responsive <br />Design</h2>
+
           <div className="space-y-6 text-xl">
             <Markdown>
               {children}
