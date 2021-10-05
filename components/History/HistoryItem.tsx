2901f88472ee20dd9a475e877d22bea9a440a10a
sanny-io <sannysherief@gmail.com>
2021-10-05 08:44:51 -0700

    Open history item in new tab.

--- components/History/HistoryItem.tsx
+++ components/History/HistoryItem.tsx
@@ -14,7 +14,7 @@ export default function HistoryItem({ name, title, description, startDate, endDa
   return (
     <div className="w-full p-4 duration-300 bg-gray-900 border border-gray-800 rounded hover:bg-gray-800">
       <h3 className={`inline-block mb-2 text-4xl font-semibold ${url ? 'hover:text-blue-500' : ''}`}>
-        <a href={url}>{name}</a>
+        <a href={url} target="_blank" rel="noopener noreferrer">{name}</a>
       </h3>
       <p className="mb-1 text-xl font-light uppercase text">{title}</p>
       <p className="mb-4">{`${startDate} - ${endDate ? endDate : 'Present'}`}</p>
