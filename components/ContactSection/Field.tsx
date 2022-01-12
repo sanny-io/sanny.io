00e2b8638f27597c780db59fb7d81c0208fa49d1
sanny-io <sannysherief@gmail.com>
2022-01-11 22:58:19 -0800

    Add transition to contact.

--- components/ContactSection/Field.tsx
+++ components/ContactSection/Field.tsx
@@ -5,7 +5,7 @@ export default function Field(props: FieldAttributes<any>) {
   return (
     <FormikField
       {...props}
-      className={`p-2 bg-gray-900 border border-gray-700 shadow-2xl outline-none focus:border-blue-500 ${props.className}`}
+      className={`p-2 bg-gray-900 border border-gray-700 shadow-2xl outline-none focus:border-blue-500 transition ${props.className}`}
     />
   )
 }
