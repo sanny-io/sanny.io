1247c7708e0bab9ec0459c19429edebc36fd91bf
sanny-io <sannysherief@gmail.com>
2022-01-24 10:10:37 -0800

    Update packages, remove formik.

--- components/ContactSection/index.tsx
+++ components/ContactSection/index.tsx
@@ -1,7 +1,7 @@
 import type { ContactFields } from '../../types'
-import React, { useState } from 'react'
-import { Formik, Form } from 'formik'
-import Field from './Field'
+import React, { FormEvent, useState } from 'react'
+
+const fieldClassName = 'p-2 bg-gray-900 border border-gray-700 shadow-2xl outline-none focus:border-blue-500 transition'
 
 type Props = {
   email: string,
@@ -9,7 +9,10 @@ type Props = {
 
 export default function ContactSection({ email }: Props) {
   const [hasError, setHasError] = useState<boolean | null>(null)
-  const handleSubmit = async (fields: ContactFields) => {
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    const fields = Object.fromEntries(new FormData(e.target as HTMLFormElement)) as ContactFields
     const response = await fetch('/api/contact', { method: 'POST', body: JSON.stringify(fields) })
 
     setHasError(response.status !== 200)
@@ -20,41 +23,34 @@ export default function ContactSection({ email }: Props) {
       <h2 className="mb-4 text-5xl tracking-widest text-center !text-secondary uppercase md:text-6xl font-light">Contact me</h2>
       <p className="text-2xl font-light text-center text-gray-400">Let's get this bread.</p>
 
-      <Formik
-        initialValues={{ name: '', email: '', message: '' } as ContactFields}
-        onSubmit={handleSubmit}
-      >
-        {() => (
-          <Form className="max-w-4xl py-8 mx-auto">
-            <div className="grid grid-cols-1 gap-4 mb-4 text-center md:grid-cols-2">
-              <Field required name="name" placeholder="Name" />
-              <Field required name="email" type="email" placeholder="Email" />
-              <Field required as="textarea" name="message" rows={5} placeholder="Message" className="md:col-span-2" />
-            </div>
+      <form className="max-w-4xl py-8 mx-auto" onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 gap-4 mb-4 text-center md:grid-cols-2">
+          <input required type="text" name="name" placeholder="Name" className={fieldClassName} />
+          <input required type="email" name="email" placeholder="Email" className={fieldClassName} />
+          <textarea required name="message" rows={5} placeholder="Message" className={`${fieldClassName} md:col-span-2`} />
+        </div>
 
-            <div>
-              <button
-                type="submit"
-                disabled={hasError === false}
-                className="block w-full mb-4 primary button md:inline-block md:w-max"
-              >
-                Submit
-              </button>
+        <div>
+          <button
+            type="submit"
+            disabled={hasError === false}
+            className="block w-full mb-4 primary button md:inline-block md:w-max"
+          >
+            Submit
+          </button>
 
-              <span
-                className={
-                  `md:ml-4 inline-block font-semibold tracking-wide ${hasError === null && 'hidden'} ${hasError ? 'text-red-500' : 'text-green-500'}`
-                }>
-                {
-                  hasError
-                    ? <>Error. Contact me directly at <a href={`mailto:${email}`} className="font-bold">{email}</a></>
-                    : <>Message received! Thanks.</>
-                }
-              </span>
-            </div>
-          </Form>
-        )}
-      </Formik>
+          <span
+            className={
+              `md:ml-4 inline-block ${hasError === null && 'hidden'} ${hasError ? 'text-red-500' : 'text-green-500'}`
+            }>
+            {
+              hasError
+                ? <>Error. Contact me directly at <a href={`mailto:${email}`} className="font-bold">{email}</a></>
+                : <>Message received! Thanks.</>
+            }
+          </span>
+        </div>
+      </form>
     </section>
   )
 }
