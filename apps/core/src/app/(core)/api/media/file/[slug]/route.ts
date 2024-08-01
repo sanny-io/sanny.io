9a4122f8bc353142968201f2245bd1b68cffb552
sanny-io <sannysherief@gmail.com>
2024-08-01 19:53:22 +0000

    Remove static handler

--- apps/core/src/app/(core)/api/media/file/[slug]/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  const path = request.url.substring(request.url.indexOf('/api/'))
-  const response = await fetch(`${process.env.NEXT_PUBLIC_DEPLOYED_URL}/${path}`)
-
-  return response
-}
-
-export const dynamic = 'force-static'
